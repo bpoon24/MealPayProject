166a2239c65764d586c9b9e2a3daece4bd17386c
Brendan Poon <bpoon24@students.polytechnic.org>
2024-02-08 15:17:28 -0800

    Wrote additional methods for Admin class.

--- Admin.java
+++ Admin.java
@@ -12,4 +12,19 @@ public class Admin {
     public Student getStudent(int myID){
         return spreadsheet[myID];
     }
+
+    public void printSpreadsheet(){
+        System.out.println(spreadsheet);
+    }
+
+    public void printNegativeBalances(){
+        for(int i = 0; i > spreadsheet.size(); i++){
+            if(spreadsheet[i].getBalance < 0){
+                System.out.print(spreadsheet[i]);
+                System.out.println();
+            }
+        }
+    }
+
+
 }
